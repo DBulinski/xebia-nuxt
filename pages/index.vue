46ca49c83027b3fbe5987933995808a7a8536cb9
Dawid Bulinski <dawid.bulinski@beamery.com>
2023-10-05 15:29:50 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Hello Xebia :)</div>
+</template>
---
+++ pages/items.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+
+const selectedItem = ref(null);
+const items = ref(new Array(5000).fill(""));
+</script>
+
+<template>
+  <strong v-if="selectedItem !== null"
+  >Selected item is {{ selectedItem + 1 }}</strong
+  >
+  <div class="items-container">
+    <div class="item" v-for="(_, index) in items">
+      <button
+          v-bind:disabled="selectedItem === index"
+          @click="selectedItem = index"
+      >
+        Index {{ index + 1 }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+.items-container {
+  display: flex;
+  gap: 12px;
+  flex-wrap: wrap;
+}
+
+.item {
+  flex: 1;
+}
+</style>
